5b636db349477455b32ddb65de1af0e00e4d7808
BazarLT <jobvital@gmail.com>
2024-11-21 12:32:21 +0200

    Initial commit of auction_platform project

--- bidding/models.py
+++ bidding/models.py
@@ -13,10 +13,11 @@ class UserProfile(models.Model):
 class Auction(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)  
     end_date = models.DateTimeField()
     seller = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
 
