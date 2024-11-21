e56abfbb0fbc2ece70d0ee8e0e1daa850d909261
BazarLT <jobvital@gmail.com>
2024-11-21 12:32:21 +0200

    Initial commit of auction_platform project

--- bidding/views.py
+++ bidding/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile, Auction
+from datetime import datetime
 
 def profile_view(request, username):
     user_profile = get_object_or_404(UserProfile, user__username=username)
@@ -7,10 +8,19 @@ def profile_view(request, username):
 
 def auction_details(request, id):
     auction = get_object_or_404(Auction, id=id)
+    
+    if isinstance(auction.end_date, datetime):
+        auction.end_date = auction.end_date.isoformat()
+    
     return render(request, 'bidding/auction_details.html', {'auction': auction})
 
 def auction_list(request):
     auctions = Auction.objects.all()
+    
+    for auction in auctions:
+        if isinstance(auction.end_date, datetime):
+            auction.end_date = auction.end_date.isoformat()
+    
     return render(request, 'bidding/auction_list.html', {'auctions': auctions})
 
 def home(request):
