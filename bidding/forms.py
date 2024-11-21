0efd9389704d259d7f63e9ba5269625ee48c475f
BazarLT <jobvital@gmail.com>
2024-11-21 13:14:36 +0200

    Add UserProfile model, Job model, and update forms

--- bidding/forms.py
+++ bidding/forms.py
@@ -1,12 +1,22 @@
 from django import forms
-from .models import Auction, Bid
+from .models import Auction, Bid, UserProfile, Job
 
 class AuctionForm(forms.ModelForm):
     class Meta:
         model = Auction
-        fields = ['title', 'description', 'start_bid', 'end_date']
+        fields = ['title', 'description', 'price', 'end_date']
 
 class BidForm(forms.ModelForm):
     class Meta:
         model = Bid
         fields = ['bid_amount', 'bidder_name']
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['bio', 'location', 'birth_date', 'profile_picture']
+
+class JobForm(forms.ModelForm):
+    class Meta:
+        model = Job
+        fields = ['title', 'description', 'start_price', 'end_date']
