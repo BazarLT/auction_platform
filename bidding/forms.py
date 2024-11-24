29ecb606e891439154195ca0839958e903f2cc9e
BazarLT <jobvital@gmail.com>
2024-11-24 18:56:20 +0200

    git commit -m "Your commit message"

--- bidding/forms.py
+++ bidding/forms.py
@@ -1,10 +1,11 @@
 from django import forms
+from django.contrib.auth.models import User  # Add this import statement
 from .models import Auction, Bid, UserProfile, Job
 
 class AuctionForm(forms.ModelForm):
     class Meta:
         model = Auction
-        fields = ['title', 'description', 'price', 'end_date']
+        fields = ['title', 'description', 'starting_bid', 'price', 'end_date']  # Added 'starting_bid'
 
 class BidForm(forms.ModelForm):
     class Meta:
@@ -14,7 +15,19 @@ class BidForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['bio', 'location', 'birth_date', 'profile_picture']
+        fields = ['phone_number', 'email', 'bio', 'location', 'birth_date', 'profile_picture']  # Added 'phone_number' and 'email'
+
+class UserRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
 
 class JobForm(forms.ModelForm):
     class Meta:
--- bidding/models.py
+++ bidding/models.py
@@ -3,11 +3,19 @@ from django.contrib.auth.models import User
 
 # Extending the User model with UserProfile
 class UserProfile(models.Model):
+    USER_ROLES = (
+        ('client', 'Client'),
+        ('guest', 'Guest'),
+        ('master', 'Master'),
+    )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=6, choices=USER_ROLES, default='guest')
     bio = models.TextField(blank=True)
     location = models.CharField(max_length=100, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    phone_number = models.CharField(max_length=15, blank=True)  # Added phone number field
+    email = models.EmailField(blank=True)  # Added email field
 
     def __str__(self):
         return self.user.username
@@ -16,6 +24,7 @@ class UserProfile(models.Model):
 class Auction(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)  # Added starting_bid field
     price = models.DecimalField(max_digits=10, decimal_places=2)
     end_date = models.DateTimeField()
     seller = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
