ad5c293b64fff3ab83b9ef100b1a6ab85dd67ca2
BazarLT <jobvital@gmail.com>
2024-11-26 08:35:53 +0200

    Verified and tested URL configurations and views

--- auction_platform/settings.py
+++ auction_platform/settings.py
@@ -1,4 +1,4 @@
-import os 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -13,11 +13,9 @@ SECRET_KEY = 'django-insecure-(5^8+khuy0@v$*c&ehk1d+vjp3du3al_%r8)#f^ki&m=8z%#6v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = []
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -31,7 +29,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'bidding',
 ]
-
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
@@ -52,7 +49,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',  # Add this line
 ]
-
 
 ROOT_URLCONF = 'auction_platform.urls'
 
@@ -76,7 +72,6 @@ WSGI_APPLICATION = 'auction_platform.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -86,7 +81,6 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -104,27 +98,23 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- bidding/urls.py
+++ bidding/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, auction_list, auction_details, create_profile_view, place_bid_view, post_job_offer_view, register_view, order_post_view, profile_view, register_as_master
+from .views import home, auction_list, auction_details, create_profile_view, place_bid_view, post_job_offer_view, register_view, order_post_view, profile_view, register_as_master, test_view
 
 urlpatterns = [
     path('', home, name='home'),
@@ -8,8 +8,10 @@ urlpatterns = [
     path('profile/create/', create_profile_view, name='create_profile'),
     path('auctions/<int:id>/bid/', place_bid_view, name='place_bid'),
     path('job/post/', post_job_offer_view, name='post_job_offer'),
-    path('profile/register/', register_view, name='register_view'),  # Ensure this comes before the profile view
+    path('profile/register/', register_view, name='register_view'),
     path('order/post/', order_post_view, name='order_post_view'),
-    path('profile/<str:username>/', profile_view, name='profile_view'),  # This should come after
-    path('register-as-master/', register_as_master, name='register_as_master'),  # Added path for registering as master
+    path('profile/<str:username>/', profile_view, name='profile_view'),
+    path('register-as-master/', register_as_master, name='register_as_master'),
+    path('test/', test_view, name='test_view'),
 ]
+
