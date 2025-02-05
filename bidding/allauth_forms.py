56c253f644d99d70fc1284562e6a2a385a0caa54
Gediminas Paulauskas <gediminas@menesis.lt>
2025-02-05 18:54:47 +0200

    Fix signup

--- bidding/allauth_forms.py
+++ bidding/allauth_forms.py
@@ -7,8 +7,6 @@ class SignUpForm(forms.Form):
     last_name = forms.CharField(max_length=30, required=True)
 
     def signup(self, request, user):
-        from allauth.account.forms import SignupForm
-        user = SignupForm().signup(request, user)
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.save()
